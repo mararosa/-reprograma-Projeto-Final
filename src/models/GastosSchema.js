46c70b0e79b4a687bd5b2e463a8b381e6548e89f
Mara Rosa <edimara.rosa@hotmail.com>
2019-12-12 11:35:10 -0300

    add default date.now no schema

--- src/models/GastosSchema.js
+++ src/models/GastosSchema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const GastosSchema = new Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
-  data: { type: Date, required: true },
+  data: { type: Date, default: Date.now  },
   motivo: { type: String, required: true },
   valor: { type: Number, required: true },
 })
